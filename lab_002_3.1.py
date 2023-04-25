01ec0a39d48a8409e7ac1b6a4d43fe85163b4644
Vadim Voytyuk <vadimvoytyuk@gmail.com>
2023-04-25 06:26:54 +0300

    ДЗ 3. Потрібно "перевернути" 5-ти значне число(1)

---
+++ lab_002_3.1.py
@@ -0,0 +1,7 @@
+number = int(input("Enter number 5 digit number:"))
+a = number//10000
+b=(number-a*10000)//1000
+c=(number-a*10000-b*1000)//100
+d= (number-a*10000-b*1000-c*100)//10
+e= number%10
+print(e,d,c,b,a)
